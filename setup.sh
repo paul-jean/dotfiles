6945a01095759db12a07cbf712ac8d62813e2a18
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-01-13 17:34:28 -0500

    local postgres. vim configs

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ rake
 ln -sf $dir/vimrc.local ~/.vimrc.local
 ln -sf $dir/tmux.conf.local ~/.tmux.conf.local
 ln -sf $dir/vimrc.bundles.local ~/.vimrc.bundles.local
+ln -sf $dir/bash_profile ~/.bash_profile
