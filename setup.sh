1363fd9a20c3d7f84468d844c6464b1aa115b944
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-01-13 19:13:03 -0500

    symlink .vimrc properly

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 dir=$PWD
 
 rake
+ln -sf $dir/vimrc ~/.vimrc
 ln -sf $dir/vimrc.local ~/.vimrc.local
 ln -sf $dir/tmux.conf.local ~/.tmux.conf.local
 ln -sf $dir/vimrc.bundles.local ~/.vimrc.bundles.local
