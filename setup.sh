7dc95dc1969ad2b4e687ec82152288f0a4783c88
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-07-13 13:38:34 -0500

    Add bundles. Dont need to add symlinks since the ruby installer does that.

--- setup.sh
+++ setup.sh
@@ -3,8 +3,5 @@
 dir=$PWD
 
 rake
-ln -sf $dir/vimrc ~/.vimrc
-ln -sf $dir/vimrc.local ~/.vimrc.local
-ln -sf $dir/tmux.conf.local ~/.tmux.conf.local
-ln -sf $dir/vimrc.bundles.local ~/.vimrc.bundles.local
+
 ln -sf $dir/bash_profile ~/.bash_profile
